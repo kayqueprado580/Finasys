6f1bc11fe719e6d2f503de03e77e4591fddfd94d
Kayque Prado Silva <46896994+kayqueprado580@users.noreply.github.com>
2021-07-02 15:55:20 -0300

    refactoring lists-components

--- finansys/src/app/pages/entries/entry-list/entry-list.component.ts
+++ finansys/src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 
 import { Entry } from "../shared/entry.model";
 import { EntryService } from "../shared/entry.service";
@@ -8,25 +9,9 @@ import { EntryService } from "../shared/entry.service";
   templateUrl: './entry-list.component.html',
   styleUrls: ['./entry-list.component.css']
 })
-export class EntryListComponent implements OnInit {
-  entries: Entry[] = [];
-  constructor(private entryService: EntryService) { }
+export class EntryListComponent extends BaseResourceListComponent <Entry> {
+  constructor(private entryService: EntryService) {
+    super(entryService)
+   }
 
-  ngOnInit() {
-    this.entryService.getAll().subscribe(
-      entries => this.entries = entries.sort((a,b)=> b.id - a.id),
-      error => alert("Erro ao carregar a lista")
-    )
-  }
-
-  deleteEntry(entry){
-    const mustDelete = confirm('Deseja realmente excluir este item?')
-
-    if(mustDelete){
-      this.entryService.delete(entry.id).subscribe(
-      () => this.entries = this.entries.filter(element => element != entry),
-      () => alert("Erro ao tentar excluir")
-      )
-    }
-  }
 }
