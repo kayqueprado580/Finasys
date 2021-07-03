9526e2ce7cb373b5609052a58aa12cd0bd1377b3
Kayque Prado Silva <46896994+kayqueprado580@users.noreply.github.com>
2021-07-03 15:50:38 -0300

    adding pages reports

--- finansys/src/app/pages/reports/reports.module.ts
+++ finansys/src/app/pages/reports/reports.module.ts
@@ -5,11 +5,13 @@ import { SharedModule } from "../../shared/shared.module";
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports/reports.component';
 
+import { ChartModule } from "primeng/chart";
 @NgModule({
   declarations: [ReportsComponent],
   imports: [
     ReportsRoutingModule,
-    SharedModule
+    SharedModule,
+    ChartModule
   ]
 })
 export class ReportsModule { }
